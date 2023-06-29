c965fd541740381fd7c6821a80820e39b59e7c19
jack-rss <119360295+jack-rss@users.noreply.github.com>
2023-06-29 16:21:52 -0400

    Update scheduling-app.js

--- scheduling-app/src/js/scheduling-app.js
+++ scheduling-app/src/js/scheduling-app.js
@@ -67,12 +67,11 @@ function slotHTML(id, type, start, end) {
              "<h5 class='card-title'>" + type + '</h5>' +
              "<p class='card-text'>Start: " + prettyStart + '</p>' +
              "<p class='card-text'>End: " + prettyEnd + '</p>' +
-             "<a href='javascript:void(0);' class='card-link' onclick='appointmentCreate(\"" +
-               slotReference + "\", \"Patient/12508016\");'>Book</a>" +
+             "<a href='javascript:void(0);' class='card-link' onclick='askForPatient(\"" +
+               slotReference + '", "' + type + '", "' + prettyStart + '", "' + prettyEnd + "\");'>Book</a>" +
            '</div>' +
          '</div>';
 }
-
 function renderSlots(slotsHTML) {
   clearUI();
   $('#slots').html(slotsHTML);
